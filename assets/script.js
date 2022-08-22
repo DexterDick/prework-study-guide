8ce63479a4ae78547d71c26e8370a7442560e8bd
Dexter Dick <@dexterdick@hotmail.com>
2022-08-22 04:33:01 -0400

    added JS file

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var shape = "circle";
+console.log(topics);
+var topics = "HTML, CSS, Git, JavaScript";
+if (0) {
+  console.log("I'm 0!");
+} else if (null) {
+  console.log("I'm null!");
+} else if (true) {
+  console.log("I'm true!");
+}
